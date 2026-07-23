50d4cb509a72031b6ade2f93ddea876bf9aad5bd
Vinicius <viniciusoliveira.270304@gmail.com>
2026-07-23 15:10:23 -0300

    feat: add POO example

---
+++ desafios/desafio_codigo_basico.rb
@@ -0,0 +1,6 @@
+number = gets.to_i
+#TODO: print no console a frase com o resultado da operação
+
+number = number + 2
+
+puts "O resultado foi: #{number}"
---
+++ vovo/loja.rb
@@ -0,0 +1,10 @@
+class Loja
+  def initialize (produto, preco)
+    @produto = produto
+    @preco = preco
+  end
+
+  def comprar
+    puts "Você comprou o produto #{@produto} pelo valor de #{@preco}"
+  end
+end
